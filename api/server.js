a7e95ef81e66214576d207d8af11a8a91866930e
Feliiks <sobrero.ludovic@gmail.com>
2022-02-22 10:41:03 +0100

    OAuth api, dotenv file

--- api/server.js
+++ api/server.js
@@ -1,21 +1,22 @@
-require('dotenv').config({ path: '../.env' });
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
-const https = require('https')
-const fs = require("fs")
 const cors = require('cors');
-const stripe = require('stripe')('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')
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 const nodemailer = require('nodemailer')
 const hbs = require('nodemailer-express-handlebars')
+const { google } = require("googleapis");
+
+const oAuth2Client = new google.auth.OAuth2(process.env.MAIL_CLIENT_ID, process.env.MAIL_CLIENT_SECRET, process.env.MAIL_REDIRECT_URI);
+oAuth2Client.setCredentials({ refresh_token: process.env.MAIL_REFRESH_TOKEN });
 
 
 // SERVER CONFIG _____________________________________________________
 
 const app = express();
-const PORT = 5000
 
-app.listen(PORT, () => {
-  console.log(`Le serveur est lancé sur le port ${PORT}.`);
+app.listen(process.env.PORT, () => {
+  console.log(`Le serveur est lancé sur le port ${process.env.PORT}.`);
 });
 
 app.use(
@@ -29,27 +30,56 @@ app.use(express.urlencoded({ extended: true }));
 
 // NODEMAILER _______________________________________________________
 
-const transporter = nodemailer.createTransport({
-  service: "gmail",
-  host: 'smtp.gmail.email',
-  auth: {
-    user: 'sobrero.ludovic@gmail.com',
-    pass: '.G87gx9p135:'
-  },
-  tls: {
-    rejectUnauthorized: false
+const sendEmailProducts = async (toEmail, products) => {
+  try {
+    const accessToken = await oAuth2Client.getAccessToken();
+
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        type: "OAuth2",
+        user: "sobrero.ludovic@gmail.com",
+        clientId: process.env.MAIL_CLIENT_ID,
+        clientSecret: process.env.MAIL_CLIENT_SECRET,
+        refreshToken: process.env.MAIL_REFRESH_TOKEN,
+        accessToken: accessToken
+      },
+      tls: {
+        rejectUnauthorized: false
+      }
+    })
+
+    const handlebarOptions = {
+      viewEngine: {
+        partialsDir: path.join(__dirname, '../storage/handlebars'),
+        defaultLayout: false,
+      },
+      viewPath: path.join(__dirname, '../storage/handlebars'),
+    };
+
+    transporter.use('compile', hbs(handlebarOptions))
+
+    let mailOptions = {
+      from: "Andrea SOBRERO <andrea.naturopathie@gmail.com>",
+      to: toEmail,
+      subject: "Tes achats sur andrea-naturopathe.com",
+      template: 'purchase',
+      attachments: []
+    }
+
+    Object.keys(products).map(product => {
+      mailOptions.attachments.push({
+        filename: products[product].name,
+        path: path.join(__dirname, '../storage', products[product].file),
+        contentType: 'application/pdf'
+      })
+    })
+
+    return transporter.sendMail(mailOptions)
+  } catch (error) {
+    console.log(error)
   }
-});
-
-const handlebarOptions = {
-  viewEngine: {
-    partialsDir: path.join(__dirname, '../storage/handlebars'),
-    defaultLayout: false,
-  },
-  viewPath: path.join(__dirname, '../storage/handlebars'),
-};
-
-transporter.use('compile', hbs(handlebarOptions))
+}
 
 
 // ROUTES ___________________________________________________________
@@ -79,23 +109,7 @@ app.post('/send-products-by-email', async (req, res) => {
   const { email, products } = req.body;
 
   try {
-    let message = {
-      from: "'Ludovic SOBRERO' <sobrero.ludovic@gmail.com>",
-      to: email,
-      subject: "Tes achats sur andreasobrero-naturopathe.com",
-      template: 'purchase',
-      attachments: []
-    }
-
-    Object.keys(products).map(product => {
-      message.attachments.push({
-        filename: products[product].name,
-        path: path.join(__dirname, '../storage', products[product].file),
-        contentType: 'application/pdf'
-      })
-    })
-
-    await transporter.sendMail(message)
+    await sendEmailProducts(email, products)
 
     res.sendStatus(200)
   } catch (err) {
